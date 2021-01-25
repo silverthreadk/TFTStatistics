6a0e7eb3da20339bdb3d1f6c1eddf38649067d09
silverthreadk <eunsill91@gmail.com>
2021-01-25 22:38:50 +0900

    Update UI in SearchSummonerFragment

--- app/src/main/java/com/silverthread/tftstatistics/ui/summoner/SearchSummonerFragment.kt
+++ app/src/main/java/com/silverthread/tftstatistics/ui/summoner/SearchSummonerFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import com.silverthread.tftstatistics.R
 import com.silverthread.tftstatistics.databinding.FragmentSearchSummonerBinding
 
 class SearchSummonerFragment : Fragment() {
@@ -45,6 +49,25 @@ class SearchSummonerFragment : Fragment() {
 
             }
         }
+
+        ArrayAdapter.createFromResource(
+                context!!,
+                R.array.region_array,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinner.adapter = adapter
+        }
+        binding.spinner.setSelection(4, false)
+        binding.spinner.setOnItemSelectedListener(object : OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        })
     }
 
     private fun showSummonor(){
