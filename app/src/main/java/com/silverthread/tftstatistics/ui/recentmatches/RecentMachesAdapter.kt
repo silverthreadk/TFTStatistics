30002c0f14b54bf1ec2a9d8ce01cf57b3321be22
silverthreadk <eunsill91@gmail.com>
2021-02-04 20:58:15 +0900

    Update text and border color depending on place

--- app/src/main/java/com/silverthread/tftstatistics/ui/recentmatches/RecentMachesAdapter.kt
+++ app/src/main/java/com/silverthread/tftstatistics/ui/recentmatches/RecentMachesAdapter.kt
@@ -28,7 +28,7 @@ class RecentMachesAdapter(private val matches: MutableList<MatchDTO>): RecyclerV
 
         fun bind(match: MatchDTO) {
             this.match = match
-
+            val context = itemView.context
             val dateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA).format(match.info?.game_datetime?.toLong())
             binding.gameDatetime.text = dateFormat
 
@@ -37,10 +37,14 @@ class RecentMachesAdapter(private val matches: MutableList<MatchDTO>): RecyclerV
             binding.gameLength.text = gameLength
             binding.type.text = if (match.info?.queue_id == "1100") itemView.context.getString(R.string.ranked) else itemView.context.getString(R.string.normal)
 
-            match.info?.participants?.filter { participant ->
-                participant.puuid == puuid
-            }?.forEach{ participant ->
+            match.info?.participants?.find { participant ->
+                participant.puuid == puuid }?.let { participant ->
                 binding.placement.text = String.format(itemView.context.getString(R.string.placement), participant.placement)
+                var color = context.getColor(context.resources.getIdentifier("rarity_" + maxOf(0,5 - participant.placement), "color", context.packageName))
+                binding.border.setBackgroundColor(color)
+                if (participant.placement > 4) color = context.getColor(R.color.material_on_surface_emphasis_medium)
+                binding.placement.setTextColor(color)
+
                 setupUnits(participant.units)
                 setupTraits(participant.traits)
             }
